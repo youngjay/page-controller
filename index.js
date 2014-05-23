f04c9bc8854385e602b6779f33095f5f9a71e0b7
yangjie <hiyoungjay@gmail.com>
2014-05-23 15:38:18 +0800

    add query parse

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var mixin = require('mixin-class');
 var ko = require('knockout');
 var pathToRegexp = require('path-to-regexp');
+var queryString = require('query-string');
+
+var parseFragment = function(str) {
+    var parts = str.split('?');
+    return {
+        path: parts[0],
+        query: parts[1] ? queryString.parse(parts[1]) : {}
+    }
+};
+
 
 var PageController = mixin(
     function(container, onChange) {
@@ -13,7 +23,8 @@ var PageController = mixin(
             var reg = pathToRegexp(route);
             var module;
             this.onChange(function(fragment) {
-                var m = reg.exec(fragment);
+                var data = parseFragment(fragment);
+                var m = reg.exec(data.path);
                 if (m) {
                     if (!module) {
                         module = moduleFactory();
@@ -22,7 +33,7 @@ var PageController = mixin(
                         container.add(module);
                     }
                     if (module.update) {
-                        module.update.apply(module, m.slice(1))
+                        module.update.apply(module, m.slice(1).concat(data.query));
                     }
                     module.__active(true);
                 }
