74d8c58c9561a38161e368f449f9c2db326aef0f
yangjie <hiyoungjay@gmail.com>
2014-06-09 11:14:13 +0800

    dd

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var mixin = require('mixin-class');
-var ko = require('knockout');
-var pathToRegexp = require('path-to-regexp');
 var queryString = require('query-string');
+var ko = require('knockout');
 
 var parseFragment = function(str) {
     var parts = str.split('?');
@@ -12,47 +11,78 @@ var parseFragment = function(str) {
 };
 
 module.exports = mixin(
-    function(container, onChange) {
-        this.container = container;
-        this.onChange = onChange;
-        var self = this;
-        onChange(function(fragment) {
-            self.fragment = fragment;
-        })
+    function() {
+        this._modules = {};
+        this._activeModules = {};
+        this._rootModule = new (require(this.pathPrefix + this.rootPath))();
     },
     {
-        add: function(route, moduleFactory) {
-            var container = this.container;
-            var reg = pathToRegexp(route);
-            var module;
+        pathPrefix: 'module/page',
 
-            var handler = function(fragment) {
-                var data = parseFragment(fragment);
-                var m = reg.exec(data.path);
-                if (m) {
-                    if (!module) {
-                        module = moduleFactory();
-                        module.__active = ko.observable(true);
-                        module.__view = '<!-- ko if: __active -->' + module.__view + '<!-- /ko -->';
-                        container.add(module);
-                    }
-                    if (module.update) {
-                        module.update.apply(module, m.slice(1).concat(data.query));
-                    }
-                    module.__active(true);
+        rootPath: '/root',
+
+        notFoundModulePath: 'module/page/not-found',
+
+        update: function(fragment) {
+            var data = parseFragment(fragment);
+            var path = data.path;
+            var query = data.query;
+            var self = this;
+
+            var parentPath = this.pathPrefix;
+            var parentModule = this._rootModule;
+
+            path.split('/').forEach(function(subPath) {
+                var fullPath = parentPath + '/' + subPath;
+
+                var currentModule = self._modules[fullPath];
+                if (!currentModule) {
+                    currentModule = self._createModule(fullPath);                       
+                    self._modules[fullPath] = currentModule;
+                    parentModule.add(currentModule);
                 }
-                else {
-                    if (module) {
-                        module.__active(false)
+            
+                var lastActiveModule = self._activeModules[parentPath];
+
+                if (lastActiveModule !== currentModule) {
+                    if (lastActiveModule) {
+                        lastActiveModule.__active(false);
                     }
+                    currentModule.__active(true);
+                    self._activeModules[parentPath] = currentModule;
                 }
-            };
 
-            this.onChange(handler);
+                if (currentModule.update) {
+                    currentModule.update(query, path);
+                }
 
-            if (this.fragment) {
-                handler(this.fragment);
+                // for next iteration
+                parentPath = fullPath;
+                parentModule = currentModule;
+            });
+        },
+
+        _createModule: function(path) {
+            var CurrentModuleFactory;
+
+            try {
+                CurrentModuleFactory = require(path);
             }
+            catch (e) {
+                CurrentModuleFactory = require(this.notFoundModulePath);
+            }
+
+            var currentModule = new CurrentModuleFactory();
+            
+            currentModule.__active = ko.observable(true);
+            currentModule.__view = '<!-- ko if: __active -->' + currentModule.__view + '<!-- /ko -->';
+
+            return currentModule;
+        },
+
+        render: function(el) {
+            el.innerHTML = this._rootModule.__view;
+            ko.applyBindings(this._rootModule, el);
         }
     }
 );
